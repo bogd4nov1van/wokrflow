eba96ca84eef4cd4e303260eabf066f6c49a8f08
i.bogdanov <i.bogdanov@tinkoff.ru>
2020-11-24 12:54:56 +0300

    fix

--- git/rebase.sh
+++ git/rebase.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-git -C $PWD pull origin master &&
+curr_branch=$(git rev-parse --abbrev-ref HEAD)
+
+git -C $PWD checkout master
+git -C $PWD pull
+git -C $PWD checkout $curr_branch
 git -C $PWD rebase master
