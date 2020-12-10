2b0edf85675906ad154f686d7e8a416d6dad4fe4
i.bogdanov <i.bogdanov@tinkoff.ru>
2020-12-10 19:46:28 +0300

    bugfix/hotfix

---
+++ git/bugfix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$(/usr/local/bin/new_branch.sh "bugfix" $1)
--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ cp git/push.sh /usr/local/bin/push
 cp git/rebase.sh /usr/local/bin/rebase
 cp git/squash.sh /usr/local/bin/squash
 cp git/delete.sh /usr/local/bin/delbranch
+cp git/new_branch.sh /usr/local/bin/new_branch.sh
+cp git/bugfix.sh /usr/local/bin/bugfix
+cp git/hotfix.sh /usr/local/bin/hotfix
 
 cp docker/dexec.sh /usr/local/bin/dcc
 cp docker/dctrash.sh /usr/local/bin/dct
